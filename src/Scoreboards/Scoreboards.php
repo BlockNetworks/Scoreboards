592b79e3084217ee7ec038481dc98fd0ec906b30
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-12-05 17:39:29 -0800

    Update Scoreboards.php

--- src/Scoreboards/Scoreboards.php
+++ src/Scoreboards/Scoreboards.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Scoreboards;
@@ -11,23 +12,23 @@ use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
-class Scoreboards extends PluginBase{
+class Scoreboards extends PluginBase {
 
 	/** @var Scoreboards $instance */
 	private static $instance;
 	/** @var array $scoreboards */
 	private $scoreboards = [];
 
-	public function onLoad(): void{
+	public function onLoad() : void {
 		self::$instance = $this;
 	}
 
-	public static function getInstance(): Scoreboards{
+	public static function getInstance() : Scoreboards {
 		return self::$instance;
 	}
 
-	public function new(Player $player, string $objectiveName, string $displayName): void{
-		if(isset($this->scoreboards[$player->getName()])){
+	public function new(Player $player, string $objectiveName, string $displayName) : void {
+		if (isset($this->scoreboards[$player->getName()])) {
 			$this->remove($player);
 		}
 		$pk = new SetDisplayObjectivePacket();
@@ -40,7 +41,7 @@ class Scoreboards extends PluginBase{
 		$this->scoreboards[$player->getName()] = $objectiveName;
 	}
 
-	public function remove(Player $player): void{
+	public function remove(Player $player) : void {
 		$objectiveName = $this->getObjectiveName($player);
 		$pk = new RemoveObjectivePacket();
 		$pk->objectiveName = $objectiveName;
@@ -48,13 +49,13 @@ class Scoreboards extends PluginBase{
 		unset($this->scoreboards[$player->getName()]);
 	}
 
-	public function setLine(Player $player, int $score, string $message): void{
-		if(!isset($this->scoreboards[$player->getName()])){
+	public function setLine(Player $player, int $score, string $message) : void {
+		if (!isset($this->scoreboards[$player->getName()])) {
 			$this->getLogger()->error("Cannot set a score to a player with no scoreboard");
 			return;
 		}
-		if($score > 15 || $score < 1){
-			$this->getLogger()->error("Score must be between the value of 1-15. $score out of range");
+		if ($score > 15 || $score < 1) {
+			$this->getLogger()->error("Score must be between the value of 1-15. " . $score . " out of range");
 			return;
 		}
 		$objectiveName = $this->getObjectiveName($player);
@@ -70,11 +71,14 @@ class Scoreboards extends PluginBase{
 		$player->sendDataPacket($pk);
 	}
 
-	public function getObjectiveName(Player $player): ?string{
+	public function getObjectiveName(Player $player) : ?string {
 		return isset($this->scoreboards[$player->getName()]) ? $this->scoreboards[$player->getName()] : null;
 	}
 
-	public function onQuit(PlayerQuitEvent $event): void{
-		if(isset($this->scoreboards[($player = $event->getPlayer()->getName())])) unset($this->scoreboards[$player]);
+	public function onQuit(PlayerQuitEvent $event): void {
+		if (isset($this->scoreboards[($player = $event->getPlayer()->getName())])) {
+			unset($this->scoreboards[$player]);
+		}
 	}
+
 }
